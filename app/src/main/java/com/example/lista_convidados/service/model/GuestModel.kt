517baf34a8ff2168061fb660592fb66d72557b6a
MiltonDevSis <milton.dev.sis@gmail.com>
2021-01-29 11:53:58 -0300

    DB update

--- app/src/main/java/com/example/lista_convidados/service/model/GuestModel.kt
+++ app/src/main/java/com/example/lista_convidados/service/model/GuestModel.kt
@@ -1,6 +1,7 @@
 package com.example.lista_convidados.service.model
 
 data class GuestModel(
+    val id: Int,
     var nome: String,
     var presence: Boolean) {
 }
--- app/src/main/java/com/example/lista_convidados/service/repository/GuestRepository.kt
+++ app/src/main/java/com/example/lista_convidados/service/repository/GuestRepository.kt
@@ -43,6 +43,7 @@ class GuestRepository private constructor(context: Context){
             val contentValue = ContentValues()
             contentValue.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.nome)
             contentValue.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+
             db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, contentValue)
             true
         }catch (e: Exception){
@@ -50,7 +51,23 @@ class GuestRepository private constructor(context: Context){
         }
     }
 
-    fun update(guest: GuestModel) {}
+    fun update(guest: GuestModel): Boolean{
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
+
+            val contentValue = ContentValues()
+            contentValue.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.nome)
+            contentValue.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, contentValue, selection, args)
+            true
+        }catch (e: Exception){
+            false
+        }
+    }
 
     fun delete(guest: GuestModel) {}
 }
