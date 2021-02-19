81fdc0f0bfe7d41e71f553850078c4f9ba6ef2a4
MiltonDevSis <milton.dev.sis@gmail.com>
2021-02-19 17:32:28 -0300

    Listagem de todos os convidados part.01

--- app/src/main/java/com/example/lista_convidados/view/AllGuestFragment.kt
+++ app/src/main/java/com/example/lista_convidados/view/AllGuestFragment.kt
@@ -8,8 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.lista_convidados.R
+import com.example.lista_convidados.view.adapter.GuestAdapter
 import com.example.lista_convidados.viewmodel.AllGuestViewModel
+import kotlinx.android.synthetic.main.fragment_all_guest.*
 
 class AllGuestFragment : Fragment() {
 
@@ -20,13 +24,19 @@ class AllGuestFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        all_guest_ViewModel =
-                ViewModelProvider(this).get(AllGuestViewModel::class.java)
+        all_guest_ViewModel = ViewModelProvider(this).get(AllGuestViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_all_guest, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        all_guest_ViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        //RecyclerView
+        //1 - obter o recycler
+        val recycler: RecyclerView = root.findViewById(R.id.recycler_All_Guests)
+
+        //2 - definir um layout
+        recycler.layoutManager = LinearLayoutManager(context)
+
+        //3 - definir um adapter
+        recycler.adapter = GuestAdapter()
+
         return root
     }
 }
---
+++ app/src/main/java/com/example/lista_convidados/view/GuestViewHolder.kt
@@ -0,0 +1,9 @@
+package com.example.lista_convidados.view
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class GuestViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+
+}
